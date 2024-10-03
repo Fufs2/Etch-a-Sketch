4fc47afa4e65c3147632ad898e4b19ae07602e0b
Fufs2 <marcelohey@hotmail.com>
2024-10-03 20:11:11 -0300

    Added the random color function

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-let newDiv = document.createElement('div');
 let destDiv = document.querySelector('.container');
 for (let i = 0; i < 256; i++) {
-    newDiv = document.createElement('div');
+   let newDiv = document.createElement('div');
     newDiv.className = 'box';
     newDiv.style.width = 'auto';
     newDiv.style.height = 'auto';
+    
+    newDiv.addEventListener('mouseover', function(){
+    newDiv.style.backgroundColor = randomColor();
+  })
+
     destDiv.appendChild(newDiv);
+    
 }
+
+function randomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+ 
